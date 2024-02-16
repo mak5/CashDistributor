36accc4a5c5f1b75f85a7cbe34e39e3e74610ab4
mak5 <med.ali.khochtali@gmail.com>
2024-02-16 11:19:46 +0100

    local master

--- CashDustributer.Tests.Unit/CashDistributorProccessor.cs
+++ CashDustributer.Tests.Unit/CashDistributorProccessor.cs
@@ -1,13 +1,28 @@
 ﻿
 namespace CashDustributor.Tests.Unit;
 
-internal class CashDistributorProccessor(int cash)
+internal class CashDistributorProccessor
 {
-    public int Cash { get; internal set; } = cash;
+    public CashDistributorProccessor(int cash)
+    {
+        if (cash % 5 != 0)
+        {
+            throw new ArgumentException("cash should be multiple of five", nameof(cash));
+        }
+
+        Cash = cash;
+    }
+
+    public int Cash { get; internal set; }
 
     public void AddCash(int cashToAdd)
     {
         ArgumentOutOfRangeException.ThrowIfNegativeOrZero(cashToAdd);
+
+        if (cashToAdd % 5 != 0)
+        {
+            throw new ArgumentException("cash should be multiple of five", nameof(cashToAdd));
+        }
 
         Cash += cashToAdd;
     }
--- CashDustributer.Tests.Unit/CashDistributorProccessorTests.cs
+++ CashDustributer.Tests.Unit/CashDistributorProccessorTests.cs
@@ -3,17 +3,21 @@ namespace CashDustributor.Tests.Unit
     public class CashDistributorProccessorTests
     {
         private int _cashToAdd;
+        private int _existingCash;
+        private CashDistributorProccessor _distributor;
 
         public CashDistributorProccessorTests()
         {
             _cashToAdd = 1000;
+            _existingCash = 10000;
+            _distributor = new CashDistributorProccessor(_existingCash);
         }
 
         [Fact]
         public void Should_CreateAnInstanceAndSetProperties_When_ParametersPassedToContructor()
         {
             // Arrange
-            var cash = 10000;
+            var cash = 5000;
             CashDistributorProccessor distributor;
 
             // Act
@@ -28,30 +32,51 @@ namespace CashDustributor.Tests.Unit
         public void Should_IncreaseCash_When_AddCachMethodGetCalled()
         {
             // Arrange
-            var existingCash = 10000;
-            var expectedResult = 11000;
-            var distributor = new CashDistributorProccessor(existingCash);
+            var expectedResult = _existingCash + _cashToAdd;
 
             // Act
-            distributor.AddCash(_cashToAdd);
+            _distributor.AddCash(_cashToAdd);
 
             // Assert
-            Assert.Equal(expectedResult, distributor.Cash);
+            Assert.Equal(expectedResult, _distributor.Cash);
         }
 
-        [Fact]
-        public void Should_ThrowsException_When_CashToAddIsNegativeOrZero()
+        [Theory]
+        [InlineData(0)]
+        [InlineData(-1)]
+        public void Should_ThrowsException_When_CashToAddIsNegativeOrZero(int cashToAdd)
         {
-            // Arrange
-            var existingCash = 10000;
-            _cashToAdd = -1;
-            var distributor = new CashDistributorProccessor(existingCash);
-
             // Act
-            Action act = () => distributor.AddCash(_cashToAdd);
+            Action act = () => _distributor.AddCash(cashToAdd);
 
             // Assert
             var ex = Assert.Throws<ArgumentOutOfRangeException>(act);
+            Assert.Equal("cashToAdd", ex.ParamName);
+        }
+
+        [Fact]
+        public void Should_ThrowsException_When_CachValue_Not_Multiple_Of_Five()
+        {
+
+            // Act
+            Action act = () => _distributor = new CashDistributorProccessor(1001);
+
+            // Assert
+            var ex = Assert.Throws<ArgumentException>(act);
+            Assert.Equal("cash", ex.ParamName);
+        }
+
+        [Fact]
+        public void Should_ThrowsException_When_AddCachValueIs_Not_Multiple_Of_Five()
+        {
+            // Arrange
+            var cashToAdd = 7;
+
+            // Act
+            Action act = () => _distributor.AddCash(cashToAdd);
+
+            // Assert
+            var ex = Assert.Throws<ArgumentException>(act);
             Assert.Equal("cashToAdd", ex.ParamName);
         }
     }
